bda780e46cb55a541cb2bc15c111446f4f644cab
Ross Byrne <ross.byrne@outlook.ie>
2016-06-18 13:45:23 +0100

    Trying to add shooting script

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject projectile;
+
+	public float fireDelay = 0.25f;
+	public float cooldownTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		cooldownTimer = fireDelay;
+
+		if (Input.GetMouseButtonDown (0) && cooldownTimer <= 0) {
+
+
+			// fire projectile
+			GameObject newProjectile = Instantiate (projectile, transform.position, transform.rotation) as GameObject;
+
+		}
+	
+	}
+}
