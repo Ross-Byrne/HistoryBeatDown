fe00d03e5d1675411da23f9c7b1a51b1eec7d394
Alan S <alstew13@hotmail.com>
2016-06-18 19:12:47 +0100

    UI stuff

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public Text clockCountDown;
+
+	private float time = 90.0f;
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		time -= Time.deltaTime;
+
+		float mins = time / 60;
+		float secs = time % 60;
+
+		clockCountDown.text = string.Format ("{0:00} : {1:00}", mins, secs);
+	}
+}
